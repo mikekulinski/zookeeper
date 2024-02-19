0aa9cfd3f49a77ca706e85c75c37541af90e5543
Mike Kulinski <mikekulinski17@gmail.com>
2024-02-19 13:50:05 -0800

    Added path validation and parsing

---
+++ pkg/server_test.go
@@ -0,0 +1,66 @@
+package pkg
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidatePath(t *testing.T) {
+	tests := []struct {
+		name          string
+		path          string
+		errorExpected bool
+	}{
+		{
+			name:          "empty string",
+			path:          "",
+			errorExpected: true,
+		},
+		{
+			name:          "not starting at root",
+			path:          "node/other/one",
+			errorExpected: true,
+		},
+		{
+			name:          "not ending with node name",
+			path:          "/a/b/",
+			errorExpected: true,
+		},
+		{
+			name:          "not ending with node name",
+			path:          "/a/b/",
+			errorExpected: true,
+		},
+		{
+			name:          "root",
+			path:          "/",
+			errorExpected: true,
+		},
+		{
+			name:          "no parents",
+			path:          "/x",
+			errorExpected: false,
+		},
+		{
+			name:          "multiple parents",
+			path:          "/x/y/z",
+			errorExpected: false,
+		},
+		{
+			name:          "empty name between path separator",
+			path:          "//y/z",
+			errorExpected: true,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := validatePath(test.path)
+			if test.errorExpected {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
