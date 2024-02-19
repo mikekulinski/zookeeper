1fd6ba1cb4b4f206ae49b18ed34490c7bca8a061
Mike Kulinski <mikekulinski17@gmail.com>
2024-02-19 13:49:55 -0800

    Added basic structure of ZNode

---
+++ pkg/znode.go
@@ -0,0 +1,31 @@
+package pkg
+
+type ZNodeType int
+
+const (
+	ZNodeType_STANDARD ZNodeType = iota
+	ZNodeType_EPHEMERAL
+)
+
+type ZNode struct {
+	// ZNode metadata.
+	name     string
+	version  int
+	children map[string]*ZNode
+	nodeType ZNodeType
+
+	// data is the data stored here by the client.
+	data []byte
+}
+
+func NewZNode(name string, version int, nodeType ZNodeType, data []byte) *ZNode {
+	return &ZNode{
+		name:    name,
+		version: version,
+		// Init to the children to an empty map instead of nil to avoid panics when writing to
+		// a nil map.
+		children: map[string]*ZNode{},
+		nodeType: nodeType,
+		data:     data,
+	}
+}
