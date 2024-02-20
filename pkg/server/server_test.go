fb9f5e15d47b2736f04863112fa82210e21e8289
Mike Kulinski <mikekulinski17@gmail.com>
2024-02-19 16:50:11 -0800

    Added tests for delete

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/mikekulinski/zookeeper/pkg/znode"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestServer_Create(t *testing.T) {
@@ -114,6 +115,73 @@ func TestServer_Create_Sequential(t *testing.T) {
 				assert.Error(t, err)
 			} else {
 				assert.Equal(t, "new_5", actualName)
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestServer_Delete(t *testing.T) {
+	const rootChildName = "rootChild"
+	const childChildName = "childChild"
+	tests := []struct {
+		name          string
+		path          string
+		version       int
+		errorExpected bool
+	}{
+		{
+			name:          "invalid path",
+			path:          "invalid",
+			errorExpected: true,
+		},
+		{
+			name:          "parent node missing",
+			path:          "/x/y/z",
+			errorExpected: true,
+		},
+		{
+			name:          "node missing, no error",
+			path:          "/random",
+			errorExpected: false,
+		},
+		{
+			name:          "invalid version",
+			path:          "/" + rootChildName,
+			version:       24,
+			errorExpected: true,
+		},
+		{
+			name:          "valid delete, root",
+			path:          "/" + rootChildName,
+			version:       0,
+			errorExpected: false,
+		},
+		{
+			name:          "valid delete, child of existing node",
+			path:          fmt.Sprintf("/%s/%s", rootChildName, childChildName),
+			version:       0,
+			errorExpected: false,
+		},
+		{
+			name:          "valid delete, ignore version check",
+			path:          "/" + rootChildName,
+			version:       -1,
+			errorExpected: false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			zk := NewServer()
+			_, err := zk.Create("/"+rootChildName, nil)
+			require.NoError(t, err)
+			_, err = zk.Create(fmt.Sprintf("/%s/%s", rootChildName, childChildName), nil)
+			require.NoError(t, err)
+
+			err = zk.Delete(test.path, test.version)
+			if test.errorExpected {
+				assert.Error(t, err)
+			} else {
 				assert.NoError(t, err)
 			}
 		})
