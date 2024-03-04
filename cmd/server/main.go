9aa23a8831cdd9f95b918115fa6a38c41315cdca
Mike Kulinski <mikekulinski17@gmail.com>
2024-03-04 10:06:52 -0800

    Added basic integration test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	zookeeper "github.com/mikekulinski/zookeeper/pkg/server"
 	pbzk "github.com/mikekulinski/zookeeper/proto"
@@ -19,8 +23,21 @@ func main() {
 	zk := zookeeper.NewServer()
 	pbzk.RegisterZookeeperServer(s, zk)
 
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		sig := <-sigCh
+		log.Printf("got signal %v, attempting graceful shutdown", sig)
+		s.GracefulStop()
+		wg.Done()
+	}()
+
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+	wg.Wait()
+	log.Println("clean shutdown")
 }
