7e9dccae91c05787aaddb446c33c900c517aa07f
Mike Kulinski <mikekulinski17@gmail.com>
2024-03-15 12:15:39 -0500

    Implement zxid

--- TODO.md
+++ TODO.md
@@ -3,16 +3,16 @@
 - Initial service implementation on 1 process
   - Fix race conditions
     - Make all the structures in our server thread safe
-- Add an async version of the server
-  - Maybe just have an async client that calls each method in a goroutine?
-  - For FIFO client order we can use channels to implement this. And use blocking vs non-blocking channels for implementing sync / async
-- Add replication for the Zookeeper server
-  - Create a write-ahead log (WAL) that we can use as the history of all changes to the ZNodes
-    - Maybe move this to disk at some point once we have multiple different processes running
-    - Figure out how to implement snapshotting so we don't have a permanent gigantic log
+- Create a write-ahead log (WAL) that we can use as the history of all changes to the ZNodes
+  - Maybe move this to disk at some point once we have multiple different processes running
+  - Figure out how to implement snapshotting so we don't have a permanent gigantic log
+- Implement atomic broadcast (ZAB)
+  - https://zookeeper.apache.org/doc/r3.4.13/zookeeperInternals.html#sc_logging
+  - Include zxid in each transaction and response to the client
   - Implement some sort of leader election
   - Redirect all writes to the leader
   - Use two-phase commit for replication
-  - Implement atomic broadcast (ZAB)
-- Split Zookeeper into a multiple processes
-  - Set up a way to run integration tests
+  - Add connect request with the last zxid we saw so that we can use to wait until the server we're connecting to is caught up
+- Add an async version of the server
+  - Maybe just have an async client that calls each method in a goroutine?
+  - For FIFO client order we can use channels to implement this. And use blocking vs non-blocking channels for implementing sync / async
---
+++ pkg/server/zab.go
@@ -0,0 +1,43 @@
+package server
+
+/*
+The ZXID has two parts: the epoch and a counter. In our implementation the zxid is a 64-bit number.
+We use the high order 32-bits for the epoch and the low order 32-bits for the counter.
+Because it has two parts represent the zxid both as a number and as a pair of integers, (epoch, count).
+The epoch number represents a change in leadership. Each time a new leader comes into power it will have its
+own epoch number.
+
+We have a simple algorithm to assign a unique zxid to a proposal:
+- the leader simply increments the zxid to obtain a unique zxid for each proposal.
+
+Leadership activation will ensure that only one leader uses a given epoch, so our simple algorithm guarantees that every
+proposal will have a unique id. A new leader establishes a zxid to start using for new proposals by getting the epoch,
+e, of the highest zxid it has seen and setting the next zxid to use to be (e+1, 0)
+
+Taken from the official Zookeeper documentation: https://zookeeper.apache.org/doc/r3.4.13/zookeeperInternals.html#sc_guaranteesPropertiesDefinitions
+*/
+type ZXID int64
+
+func NewZXID(epoch int32, counter int32) ZXID {
+	// Set up the epoch and counter to be lined up with the high and low 32 bits of the zxid.
+	var zxid int64 = 0
+	highBits := int64(epoch) << 32
+	lowBits := int64(counter)
+
+	// Set the high and low bits of the zxid using bitwise OR.
+	zxid |= highBits
+	zxid |= lowBits
+	return ZXID(zxid)
+}
+
+func (z ZXID) GetEpoch() int32 {
+	// Get the epoch from the higher 32 bits of the zxid.
+	return int32(z >> 32)
+}
+
+func (z ZXID) GetCounter() int32 {
+	// Get the epoch from the lower 32 bits of the zxid. We do this by creating a bit mask of the lower 32 bits
+	// and doing a bitwise AND to only get those bits.
+	var maskLow32 ZXID = 0xFFFFFFFF
+	return int32(z & maskLow32)
+}
