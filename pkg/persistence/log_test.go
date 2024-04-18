4194fa89db0929e9f19905d66b7e09229c37ab8b
Mike Kulinski <mikekulinski17@gmail.com>
2024-04-18 11:07:43 -0700

    Make log test work anywhere

--- pkg/persistence/log_test.go
+++ pkg/persistence/log_test.go
@@ -1,12 +1,28 @@
 package persistence
 
 import (
+	"log"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	cwd string
+)
+
+func init() {
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	cwd = strings.TrimSuffix(dir, "/pkg/persistence")
+	log.Println(cwd)
+}
+
 func TestLogManager_Append(t *testing.T) {
-	_, err := NewLogManager("/Users/mkulinski/src/zookeeper/logs")
+	_, err := NewLogManager(cwd + "/logs")
 	assert.NoError(t, err)
 }
